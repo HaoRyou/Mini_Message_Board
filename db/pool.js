8906e30b069f5a5bafc082bbb36c65e2c82ff76d
Hao are you <158393369+HaoRyou@users.noreply.github.com>
2025-09-12 12:19:57 -0400

    Delete db directory

--- db/pool.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-
-module.exports = new Pool({
-  host: 'localhost',
-  User: 'starting',
-  database: 'message_board',
-  password: '1140067995',
-  port: 5432,
-});
